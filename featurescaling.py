b9dd6146ccc19116adfe951d2519258a190a6c1e
knut tungland <stunglan@gmail.com>
2016-10-16 14:47:27 +0200

    working python from visual studio core

--- featurescaling.py
+++ featurescaling.py
@@ -1,9 +1,34 @@
+#%%
 import pickle
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import re
 import sys
 from operator import itemgetter
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from IPython.core.display import HTML
+
 pd.set_option('display.max_rows', 5000)
+
+#%%
+print "hello"
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show() 
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.cos(x))
+plt.show() 
+
