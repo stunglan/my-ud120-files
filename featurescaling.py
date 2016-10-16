afbaa5623e0e630571d729ec27915ccd6f9fb75b
knut tungland <stunglan@gmail.com>
2016-10-16 11:26:45 +0200

    started using visual studio code

---
+++ featurescaling.py
@@ -0,0 +1,9 @@
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import sys
+from operator import itemgetter
+from IPython.core.display import HTML
+pd.set_option('display.max_rows', 5000)
